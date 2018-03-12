fd6c77fb937ef17e5390ebd701d12f33ac5fcb6c
tony <470963660@qq.com>
2018-03-12 14:50:30 +0800

    update the baseline values

--- tactilesensors4/scripts/tactile_preprocessing.py
+++ tactilesensors4/scripts/tactile_preprocessing.py
@@ -8,29 +8,14 @@ import numpy as np
 pub = None 
 def static_data_preprocessing(data):
     global pub
-    taxels1_base_values = np.array([16884, 14131, 18246, 12081, 21928, 16342, 11646, 11801, 27208, 10953, 10925, 10974, 36376, 10554, 10482, 10776, 36159, 11215, 13934, 16362, 11855, 36329, 11364, 16047, 37449, 12054, 11610, 17657])
-    taxels2_base_values = np.array([17325, 14641, 18280, 12145, 21834, 16248, 11538, 11833, 26077, 10372, 10689, 10787, 35742, 10021, 10187, 10557, 35477, 11163, 13711, 15729, 11805, 35462, 11285, 15483, 36841, 12052, 11457, 17275])
+    taxels1_base_values = np.array([16777, 14002, 18110, 12001, 21747, 16140, 11544, 11711, 26977, 10969, 10823, 10873, 36224, 10387, 10379, 10674, 35944, 11123, 13774, 16186, 11730, 36002, 11230, 15860, 37171, 11928, 11458, 17432])
+    taxels2_base_values = np.array([17585, 14837, 18458, 12328, 21947, 16390, 11678, 11982, 26232, 10460, 10788, 10869, 35879, 10122, 10280, 10663, 35630, 11288, 13814, 15850, 11939, 35600, 11413, 15589, 37027, 12185, 11612, 17399])
     taxels1 = np.array(data.taxels[0].values) -  taxels1_base_values
     taxels2 = np.array(data.taxels[1].values) -  taxels2_base_values
-#    taxels1 = copy.deepcopy(np.subtract(data.taxels[0].values, taxels1_base_values))
-#    taxels2 = copy.deepcopy(np.subtract(data.taxels[1].values, taxels2_base_values))
-#    taxels1 = np.divide(copy.deepcopy(data.taxels[0].values), 10000.00)
-#    taxels2 = np.divide(copy.deepcopy(data.taxels[1].values), 10000.00)
-#    rospy.loginfo(data.taxels[0].values)
     taxels1_sorted = np.sort(taxels1)
     taxels2_sorted = np.sort(taxels2)
     taxels1 = taxels1_sorted[-5:]
     taxels2 = taxels2_sorted[-5:]
-
-    ''''
-    if (np.max(taxels1) or abs(np.min(taxels1))) > 1000:
-        rospy.loginfo('tactile[0] sensor noise...')
-        return
-    if (np.max(taxels2) or abs(np.min(taxels2))) > 1000:
-        rospy.loginfo('tactile[1] sensor noise...')
-        return
-    '''
-
 
     msg = tactile()    
     msg.tactile_values_0 = taxels1[0]
@@ -38,12 +23,12 @@ def static_data_preprocessing(data):
     msg.tactile_values_2 = taxels1[2]
     msg.tactile_values_3 = taxels1[3]
     msg.tactile_values_4 = taxels1[4]
-
     msg.tactile_values_5 = taxels2[0]
     msg.tactile_values_6 = taxels2[1]
     msg.tactile_values_7 = taxels2[2]
     msg.tactile_values_8 = taxels2[3]
     msg.tactile_values_9 = taxels2[4]
+    rospy.loginfo(msg)    
     pub.publish(msg)
     
 def main():
